ae52c4627484398dfa9e6713e7f88d88ab9ed73d
Cees de Groot <cees@pagerduty.com>
2015-11-17 22:00:22 -0500

    Trying out sensible tabbing defaults

--- init.el
+++ init.el
@@ -1,10 +1,10 @@
-;;
+;;  -*- coding: utf-8-unix; -*-
 ;; Function for system-dependent stuff
 ;;
 (defun system-setting (system default-setting settings-alist)
   (let ((setting (cdr (assoc system settings-alist))))
     (if setting
-      setting
+        setting
       default-setting)))
 
 (defun default-system-setting (default-setting settings-alist)
@@ -16,6 +16,13 @@
 (load (expand-file-name "~/quicklisp/slime-helper.el"))
 (setq inferior-lisp-program
       (default-system-setting "sbcl"
-	'((CASEAW . "C:\\Apps\\sbcl-1.3.0\\sbcl.exe"))))
+        '((CASEAW . "C:\\Apps\\sbcl-1.3.0\\sbcl.exe"))))
 
 
+;;
+;; Tabs are soo 1960...
+;;
+(setq-default indent-tabs-mode nil)
+(setq tab-width 4)
+(defvaralias 'c-basic-offset 'tab-width)
+(defvaralias 'cperl-indent-level 'tab-width)
